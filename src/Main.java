f5e275627b6fd26ee3548edcdc78231992618721
Admin <tomand94@gmail.com>
2021-09-26 13:01:36 +0200

    Cw2

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,24 @@ public class Main {
         *   dane mają być wprowadzane z klawiatury w konsoli
         *   b) wyświetlić dane za pomocą println i printf
         */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj imię: ");
+        String imie = scan.next();
+        System.out.println("Podaj nazwisko: ");
+        String nazwisko = scan.next();
+        System.out.println("Podaj wiek: ");
+        int wiek = scan.nextInt();
+        System.out.println("Podaj nr. indeksu: ");
+        int indeks = scan.nextInt();
 
-
+        System.out.println(imie + " " + nazwisko + ". Wiek: " + wiek + ". Indeks: " + indeks + ".");
+        System.out.printf("%s %s, wiek: %d, indeks: %d \n", imie, nazwisko, wiek, indeks);
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+        int a = 20;
+        int b = 15;
 
+        System.out.println("a+b=" + (a+b) + ", a-b=" + (a-b) + ", a*b=" + (a*b) + ", a/b=" + (a/b) + ", a%b=" + (a%b));
     }
 }
