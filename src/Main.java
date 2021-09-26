4312469c6a7f0dea1fa6d54f9d764420b30915be
Admin <tomand94@gmail.com>
2021-09-26 12:50:23 +0200

    Cw2

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,41 @@ public class Main {
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+        int zmienna1 = 5;
+        double zmienna2 = 2.4;
+        byte zmienna3 = -20;
+        boolean zmienna4 = false;
+        String zmienna5 = "Testowy string.";
+        float zmienna6 = 12.22f;
+        System.out.println(zmienna1);
+        System.out.println(zmienna2);
+        System.out.println(zmienna3);
+        System.out.println(zmienna4);
+        System.out.println(zmienna5);
+        System.out.println(zmienna6);
 
         /*
-        *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
-        *    mnożenia, dziealenia i modulo na zmiennych:
-        *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
-        *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
+         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
+         *    mnożenia, dziealenia i modulo na zmiennych:
+         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
+         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
+         *  */
+
+        int liczbaA = 15;
+        int liczbaB = 10;
+        double liczbaX = 5.5;
+        double liczbaY = 3.5;
+        int sum1 = liczbaA+liczbaB;
+        int sub1 = liczbaA-liczbaB;
+        int mul1 = liczbaA*liczbaB;
+        int div1 = liczbaA/liczbaB;
+        int mod1 = liczbaA%liczbaB;
+
+        double sum2 = liczbaX+liczbaY;
+        double sub2 = liczbaX-liczbaY;
+        double mul2 = liczbaX*liczbaY;
+        double div2 = liczbaX/liczbaY;
+        double mod2 = liczbaX%liczbaY;
 
 
 
